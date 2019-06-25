990da376307e8b9d0a7066559e691b7e57721244
Max Goodman <max.goodman@flatironschool.com>
2019-06-25 12:18:48 -0400

    adding google analytics

--- src/components/HomePageTemplate/index.js
+++ src/components/HomePageTemplate/index.js
@@ -68,6 +68,14 @@ const HomePageTemplate = ({
       </div>
       </div>
     </section>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-34842344-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-34842344-3');
+</script>
   </div>
 )
 
